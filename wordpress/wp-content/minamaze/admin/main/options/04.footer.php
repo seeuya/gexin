0fae4f7805a7749bf26c9404fc3bbbaa11adc151
seeuya <zhushen@outlook.com>
2015-08-11 17:19:05 +0800

    edit company info

--- wordpress/wp-content/minamaze/admin/main/options/04.footer.php
+++ wordpress/wp-content/minamaze/admin/main/options/04.footer.php
@@ -226,7 +226,7 @@ global $footer_w6;
 function thinkup_input_copyright() {
 global $thinkup_footer_copyright;
 
-	printf( __( '%1$s. %2$s.', 'lan-thinkupthemes' ) , '广东格新律师事务所', '版权所有'); 
+	printf( __( '%1$s %2$s', 'lan-thinkupthemes' ) , '广东格新律师事务所', '版权所有'); 
 }
 
 
